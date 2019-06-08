d4c280ea1e988325805f13e9ed860d72668a8409
Dima Krasota <g0t0@bigmir.net>
2019-06-08 18:31:42 +0300

    Added login functionality

--- django_fb/fb_auth_app/views.py
+++ django_fb/fb_auth_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home(request):
     return render(request, 'index.html')
+
+
+def login_user(request):
+    return render(request, 'login.html')
